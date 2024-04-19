9389301e3325d358654a8747ddd6c65544177f92
shuangzhiaishang <1023991873@qq.com>
2024-04-19 09:37:05 +0800

    2

--- test.py
+++ test.py
@@ -13,17 +13,17 @@ test_dir = 'shapefiles/test'
 
 def test_buffer():
     # 测试单个要素缓冲区
-    gdf = gpd.read_file('/shapefiles/poi/_长沙市/_长沙市.shp')
+    gdf = gpd.read_file('./shapefiles/poi/_长沙市/_长沙市.shp')
     buffer_size = 0.01
     buffered_gdf = create_buffer(gdf, buffer_size)
-    buffered_gdf.to_file(os.join(test_dir, 'buffer.shp'))
+    buffered_gdf.to_file(os.path.join(test_dir, 'buffer.shp'))
 
     # 测试多个要素缓冲区
     gdfs = [gdf for _ in range(5)]
     buffer_sizes = [buffer_size * i for i in range(5)]
     buffered_gdfs = create_multiple_buffers(gdfs, buffer_sizes)
     for i in range(5):
-        buffered_gdf.to_file(os.join(test_dir, f'buffer{i}.shp'))
+        buffered_gdf.to_file(os.path.join(test_dir, f'buffer{i}.shp'))
 
 
 if __name__ == "__main__":
